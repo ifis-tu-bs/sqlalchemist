07c74550ac3b965350fff021b1fae8ce16b586eb
goldenticket <n.naczk@tu-bs.de>
2015-10-08 14:28:22 +0200

    edited shop
    loft coins need to be made yet

--- front-end/assets/js/screens/shop.js
+++ front-end/assets/js/screens/shop.js
@@ -37,9 +37,10 @@ game.ShopScreen = me.ScreenObject.extend({
 
         function getShopInfo(xmlHttpRequest) {
 
-            function goBuy(id) {
+            function goBuy(id, spriteid) {
                 return function() {
                     game.data.shopId = id;
+                    game.data.spriteId = spriteid;
                     me.state.change(STATE_BUY);
                 }
             }
@@ -49,157 +50,100 @@ game.ShopScreen = me.ScreenObject.extend({
             game.data.shop = shop;
 
             for (var i = 0; i < 7; i++) {
-                if (shop[i].avatar.isTeam) {
-                    this.difference = 0.5575;
-                    this.size = 1.0151;
-                } else if (!shop[i].bought) {
-                    this.difference = 0;
-                    this.size       = 0;
+                if (!shop[i].bought) {
+                    if (shop[i].avatar.isTeam) {
+                        this.difference = 0.5575;
+                        this.size = 1.0151;
+                    } else {
+                        this.difference = 0;
+                        this.size       = 0;
+                    }
+                    var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i, shop[i].id), 4.948 + this.size, 8.433, 23.436 - this.difference, 18.129167 + i * 10.9375, 1);
+                    shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
+                    $("#shopItem" + i).fadeIn(100);
+                    me.game.world.addChild(shopItem);
                 }
-                var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i), 4.948 + this.size, 8.433, 23.436 - this.difference, 18.129167 + i * 10.9375, 1);
-                shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
-                $("#shopItem" + i).fadeIn(100);
-                me.game.world.addChild(shopItem);
             }
 
             for (var i = 7; i < 14; i++) {
-                if (shop[i].avatar.isTeam) {
-                    this.difference = 0.5575;
-                    this.size = 1.0151;
-                } else if (!shop[i].bought) {
-                    this.difference = 0;
-                    this.size       = 0;
+                if (!shop[i].bought) {
+                    if (shop[i].avatar.isTeam) {
+                        this.difference = 0.5575;
+                        this.size = 1.0151;
+                    } else {
+                        this.difference = 0;
+                        this.size       = 0;
+                    }
+                    var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i), 4.848 + this.size, 8.333, 33.330 - this.difference, 18.129167 + (i - 7) * 10.9375, 1);
+                    shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
+                    $("#shopItem" + i).fadeIn(100);
+                    me.game.world.addChild(shopItem);
                 }
-                var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i), 4.848 + this.size, 8.333, 33.330 - this.difference, 18.129167 + (i - 7) * 10.9375, 1);
-                shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
-                $("#shopItem" + i).fadeIn(100);
-                me.game.world.addChild(shopItem);
             }
 
             for (var i = 14; i < 21; i++) {
-                if (shop[i].avatar.isTeam) {
-                    this.difference = 0.5575;
-                    this.size = 1.0151;
-                } else if (!shop[i].bought) {
-                    this.difference = 0;
-                    this.size       = 0;
+                if (!shop[i].bought) {
+                    if (shop[i].avatar.isTeam) {
+                        this.difference = 0.5575;
+                        this.size = 1.0151;
+                    } else {
+                        this.difference = 0;
+                        this.size       = 0;
+                    }
+                    var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i), 4.848 + this.size, 8.333, 38.942 - this.difference, 18.129167 + (i - 14) * 10.9375, 1);
+                    shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
+                    $("#shopItem" + i).fadeIn(100);
+                    me.game.world.addChild(shopItem);
                 }
-                var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i), 4.848 + this.size, 8.333, 38.942 - this.difference, 18.129167 + (i - 14) * 10.9375, 1);
-                shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
-                $("#shopItem" + i).fadeIn(100);
-                me.game.world.addChild(shopItem);
             }
 
             for (var i = 21; i < 27; i++) {
-                if (shop[i].avatar.isTeam) {
-                    this.difference = 0.5575;
-                    this.size = 1.0151;
-                } else if (!shop[i].bought) {
-                    this.difference = 0;
-                    this.size       = 0;
+                if (!shop[i].bought) {
+                    if (shop[i].avatar.isTeam) {
+                        this.difference = 0.5575;
+                        this.size = 1.0151;
+                    } else {
+                        this.difference = 0;
+                        this.size = 0;
+                    }
+                    var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i), 4.848 + this.size, 8.333, 51.994 - this.difference, 29.06666 + (i - 21) * 10.9375, 1);
+                    shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
+                    $("#shopItem" + i).fadeIn(100);
+                    me.game.world.addChild(shopItem);
                 }
-                var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i), 4.848 + this.size, 8.333, 51.994 - this.difference, 29.06666 + (i - 21) * 10.9375, 1);
-                shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
-                $("#shopItem" + i).fadeIn(100);
-                me.game.world.addChild(shopItem);
             }
 
             for (var i = 27; i < 32; i++) {
-                if (shop[i].avatar.isTeam) {
-                    this.difference = 0.5575;
-                    this.size = 1.0151;
-                } else if (!shop[i].bought) {
-                    this.difference = 0;
-                    this.size       = 0;
+                if (!shop[i].bought) {
+                    if (shop[i].avatar.isTeam) {
+                        this.difference = 0.5575;
+                        this.size = 1.0151;
+                    } else {
+                        this.difference = 0;
+                        this.size       = 0;
+                    }
+                     var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i), 4.848 + this.size, 8.333, 57.782 - this.difference, 40.00417 + (i - 27) * 10.9375, 1);
+                    shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
+                    $("#shopItem" + i).fadeIn(100);
+                    me.game.world.addChild(shopItem);
                 }
-                var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i), 4.848 + this.size, 8.333, 57.782 - this.difference, 40.00417 + (i - 27) * 10.9375, 1);
-                shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
-                $("#shopItem" + i).fadeIn(100);
-                me.game.world.addChild(shopItem);
             }
 
             for (var i = 32; i < 38; i++) {
-                if (shop[i].avatar.isTeam) {
-                    this.difference = 0.5575;
-                    this.size = 1.0151;
-                } else if (!shop[i].bought) {
-                    this.difference = 0;
-                    this.size       = 0;
-                }
-                var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i), 4.848 + this.size, 8.333, 70.2545 - this.difference, 29.06666 + (i - 32) * 10.9375, 1);
-                shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
-                $("#shopItem" + i).fadeIn(100);
-                me.game.world.addChild(shopItem);
-            }
-
-
-
-            /**for (var i = 0; i < 7; i++) {
                 if (!shop[i].bought) {
-                    this.size = 64;
-                    this.difference = 0;
                     if (shop[i].avatar.isTeam) {
-                        this.size = 84;
-                        this.difference = 10;
+                        this.difference = 0.5575;
+                        this.size = 1.0151;
+                    } else {
+                        this.difference = 0;
+                        this.size = 0;
                     }
-                    me.game.world.addChild(new showSkin(312 - this.difference, 140 + 84 * (i - 0), shop[i].thumbnailUrl, this.size, shop[i].id, i));
+                    var shopItem = new game.ClickableElement('shopItem' + i, '', goBuy(i), 4.848 + this.size, 8.333, 70.2545 - this.difference, 29.06666 + (i - 32) * 10.9375, 1);
+                    shopItem.setImage("assets/data/img/avatare/" + shop[i].thumbnailUrl + "_front.png", "avatar");
+                    $("#shopItem" + i).fadeIn(100);
+                    me.game.world.addChild(shopItem);
                 }
             }
-             for (var i = 7; i < 14; i++) {
-                if (!shop[i].bought) {
-                    this.size = 64;
-                    this.difference = 0;
-                    if (shop[i].avatar.isTeam) {
-                        this.size = 84;
-                        this.difference = 10;
-                    }
-                    me.game.world.addChild(new showSkin(436 - this.difference, 140 + 84 * (i - 7), shop[i].thumbnailUrl, this.size, shop[i].id, i));
-                }
-            }
-             for (var i = 14; i < 21; i++) {
-                if (!shop[i].bought) {
-                    this.size = 64;
-                    this.difference = 0;
-                    if (shop[i].avatar.isTeam) {
-                        this.size = 84;
-                        this.difference = 10;
-                    }
-                    me.game.world.addChild(new showSkin(518 - this.difference, 140 + 84 * (i - 14), shop[i].thumbnailUrl, this.size, shop[i].id, i));
-                }
-            }
-             for (var i = 21; i < 27; i++) {
-                if (!shop[i].bought) {
-                    this.size = 64;
-                    this.difference = 0;
-                    if (shop[i].avatar.isTeam) {
-                        this.size = 84;
-                        this.difference = 10;
-                    }
-                    me.game.world.addChild(new showSkin(685 - this.difference, 224 + 84 * (i - 21), shop[i].thumbnailUrl, this.size, shop[i].id, i));
-                }
-            }
-             for (var i = 27; i < 32; i++) {
-                if (!shop[i].bought) {
-                    this.size = 64;
-                    this.difference = 0;
-                    if (shop[i].avatar.isTeam) {
-                        this.size = 84;
-                        this.difference = 10;
-                    }
-                    me.game.world.addChild(new showSkin(768 - this.difference, 308 + 84 * (i - 27), shop[i].thumbnailUrl, this.size, shop[i].id, i));
-                }
-            }
-            for (var i = 32; i < 38; i++) {
-                if (!shop[i].bought) {
-                    this.size = 64;
-                    this.difference = 0;
-                    if (shop[i].avatar.isTeam) {
-                        this.size = 84;
-                        this.difference = 10;
-                    }
-                    me.game.world.addChild(new showSkin(930 - this.difference, 224 + 84 * (i - 32), shop[i].thumbnailUrl, this.size, shop[i].id, i));
-                }
-            }*/
 
         }
 
