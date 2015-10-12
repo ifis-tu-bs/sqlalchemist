7bb20f4082ee38076ae4287cbea723689f8d1b29
Fabio Mazzone <fabio.mazzone@me.com>
2015-10-12 21:18:22 +0200

    Added TaskSetBootstrap

--- back-end/app/bootstrap/BootstrapDB.java
+++ back-end/app/bootstrap/BootstrapDB.java
@@ -1,13 +1,14 @@
 package bootstrap;
 
 public class BootstrapDB {
-  public static void init() {
-
-    PotionBootstrap.init();
-    ScrollBootstrap.init();
-    AvatarBootstrap.init();
-    ShopItemBootstrap.init();
-    MapBootstrap.init();
-    StoryChallengeBootstrap.init();
-  }
+    public static void init() {
+        PotionBootstrap.init();
+        ScrollBootstrap.init();
+        AvatarBootstrap.init();
+        ShopItemBootstrap.init();
+        MapBootstrap.init();
+        StoryChallengeBootstrap.init();
+        UserBootstrap.init();
+        TaskSetBootstrap.init();
+    }
 }
---
+++ back-end/app/bootstrap/TaskSetBootstrap.java
@@ -0,0 +1,133 @@
+package bootstrap;
+
+import dao.ProfileDAO;
+
+import models.Profile;
+import models.TaskSet;
+
+import view.TaskSetView;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import play.libs.Json;
+
+/**
+ * Created by fabiomazzone on 12/10/15.
+ */
+public class TaskSetBootstrap {
+    public static void init(){
+        JsonNode node = Json.parse("{\n" +
+                "    \"tableDefinitions\": [\n" +
+                "        {\n" +
+                "            \"tableName\": \"User\",\n" +
+                "            \"columns\": [\n" +
+                "                {\n" +
+                "                    \"columnName\": \"id\",\n" +
+                "                    \"dataType\": \"bigint\",\n" +
+                "                    \"primaryKey\": true,\n" +
+                "                    \"notNull\": false,\n" +
+                "                    \"datagenSet\": 0\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"columnName\": \"EMail\",\n" +
+                "                    \"dataType\": \"Varchar(255)\",\n" +
+                "                    \"primaryKey\": true,\n" +
+                "                    \"notNull\": false,\n" +
+                "                    \"datagenSet\": 0\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"columnName\": \"FirstName\",\n" +
+                "                    \"dataType\": \"Varchar(255)\",\n" +
+                "                    \"primaryKey\": false,\n" +
+                "                    \"notNull\": false,\n" +
+                "                    \"datagenSet\": 0\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"columnName\": \"LastName\",\n" +
+                "                    \"dataType\": \"Varchar(255)\",\n" +
+                "                    \"primaryKey\": false,\n" +
+                "                    \"notNull\": false,\n" +
+                "                    \"datagenSet\": 0\n" +
+                "                }\n" +
+                "\n" +
+                "            ],\n" +
+                "            \"extension\": null\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"tableName\": \"Profile\",\n" +
+                "            \"columns\": [\n" +
+                "                {\n" +
+                "                    \"columnName\": \"id\",\n" +
+                "                    \"dataType\": \"bigint\",\n" +
+                "                    \"primaryKey\": true,\n" +
+                "                    \"notNull\": false,\n" +
+                "                    \"datagenSet\": 0\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"columnName\": \"user_id\",\n" +
+                "                    \"dataType\": \"bigint\",\n" +
+                "                    \"primaryKey\": true,\n" +
+                "                    \"notNull\": false,\n" +
+                "                    \"datagenSet\": 0\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"columnName\": \"username\",\n" +
+                "                    \"dataType\": \"Varchar(255)\",\n" +
+                "                    \"primaryKey\": true,\n" +
+                "                    \"notNull\": false,\n" +
+                "                    \"datagenSet\": 0\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"extension\": null\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"tableName\": \"Mir\",\n" +
+                "            \"columns\": [\n" +
+                "                {\n" +
+                "                    \"columnName\": \"id\",\n" +
+                "                    \"dataType\": \"bigint\",\n" +
+                "                    \"primaryKey\": true,\n" +
+                "                    \"notNull\": false,\n" +
+                "                    \"datagenSet\": 0\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"columnName\": \"user_id\",\n" +
+                "                    \"dataType\": \"bigint\",\n" +
+                "                    \"primaryKey\": true,\n" +
+                "                    \"notNull\": false,\n" +
+                "                    \"datagenSet\": 0\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"columnName\": \"mirname\",\n" +
+                "                    \"dataType\": \"Varchar(255)\",\n" +
+                "                    \"primaryKey\": true,\n" +
+                "                    \"notNull\": false,\n" +
+                "                    \"datagenSet\": 0\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"extension\": null\n" +
+                "        }\n" +
+                "    ],\n" +
+                "    \"foreignKeys\": [\n" +
+                "        {\n" +
+                "            \"sourceTable\":\"Profile\",\n" +
+                "            \"sourceColumn\":\"user_id\",\n" +
+                "            \"destinationTable\":\"User\",\n" +
+                "            \"destinationColumn\":\"id\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"sourceTable\":\"Mir\",\n" +
+                "            \"sourceColumn\":\"user_id\",\n" +
+                "            \"destinationTable\":\"User\",\n" +
+                "            \"destinationColumn\":\"id\"\n" +
+                "        }\n" +
+                "    ],\n" +
+                "    \"tasks\": [],\n" +
+                "    \"isHomework\":   true\n" +
+                "}\n");
+
+        Profile profile = ProfileDAO.getByUsername("admin");
+
+        TaskSet taskSet = TaskSetView.fromJsonForm(profile, node);
+        taskSet.save();
+    }
+}
