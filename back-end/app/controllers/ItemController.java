2b0e7c5fa38260e1545283d128b40ea5fd44820a
Invisible4ever <soeren.vanderwall@web.de>
2015-11-10 18:27:01 +0100

    Fuer Nicoleee

--- back-end/app/controllers/ItemController.java
+++ back-end/app/controllers/ItemController.java
@@ -69,7 +69,7 @@ public class ItemController extends Controller {
         Logger.info("calcLevel: " + calcLevel + " | currentLevel: " + currentLevel);
 
         if(depth > 1 && !profile.isTutorialDone()) {
-            profile.setTutorialDone();
+            //profile.setTutorialDone();
         }
 
         if(calcLevel > currentLevel) {
