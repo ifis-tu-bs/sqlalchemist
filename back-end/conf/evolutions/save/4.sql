46a44ecbeba335fdbed6217962c8d1493d87ec8c
Fabio Mazzone <fabio.mazzone@me.com>
2015-11-12 16:06:50 +0100

    HomeworkSubmit is now implemented

--- back-end/conf/evolutions/save/4.sql
+++
@@ -1,8 +0,0 @@
-# --- !Ups
-ALTER TABLE taskset ADD COLUMN available tinyint(1) default 0;
-ALTER TABLE task ADD COLUMN available tinyint(1) default 0;
-
-
-# --- !Downs
-ALTER TABLE taskset DROP COLUMN available;
-ALTER TABLE task DROP COLUMN available;
