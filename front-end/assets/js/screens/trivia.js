b40688dd28ad1b1723987f787ba46020954d78d8
goldenticket <n.naczk@tu-bs.de>
2015-11-13 12:05:31 +0100

    done

--- front-end/assets/js/screens/trivia.js
+++ front-end/assets/js/screens/trivia.js
@@ -8,9 +8,12 @@ game.TriviaScreen = me.ScreenObject.extend({
          * Create background-div and add image to it.
          */
         var backgroundTrivia = new game.BackgroundElement('backgroundTriviaId', 100, 100, 0, 0, 'none');
-        backgroundTrivia.setImage("assets/data/img/gui/difficulty_screen.png", "backgroundtrivia");
+        backgroundTrivia.setImage("assets/data/img/gui/task_screen_new.png", "backgroundtrivia");
         me.game.world.addChild(backgroundTrivia);
-        $("#backgroundTriviaId").fadeIn(100);
+
+        var triviaHeader = new game.TextOutputElement('triviaHeader', 100, 7, 0, 10, 1);
+        me.game.world.addChild(triviaHeader);
+        triviaHeader.write("Choose A Difficulty:");
 
         /**
          * Function which returns the specific task difficulty from the according button and redirects the user to the
@@ -29,6 +32,7 @@ game.TriviaScreen = me.ScreenObject.extend({
                     $("#taskLevel" + i).fadeOut(100);
                 }
                 $("#backFromTrivia").fadeOut(100);
+                $("#triviaHeader").fadeOut(100);
                 setTimeout( function() {
                     me.state.change(STATE_TASK);
                 }, 100);
@@ -40,16 +44,14 @@ game.TriviaScreen = me.ScreenObject.extend({
          */
         for (var i = 0; i < 2; i++) {
             var taskLevel = new game.ClickableElement('taskLevel' + i + 1, '', this.CoinClick(i + 1), 12.5, 21.484375, 30.30303 + i * 24.242424, 26.041667, 1);
-            taskLevel.setImage("assets/data/img/buttons/task.png", "coin");
+            taskLevel.setImage("assets/data/img/buttons/difficulty_coins/coin_" + (i + 1) + ".png", "coin");
             me.game.world.addChild(taskLevel);
-            $("#taskLevel" + i + 1).fadeIn(100);
         }
 
         for (var j = 0; j < 3; j++){
             var taskLevel = new game.ClickableElement('taskLevel' + j + 3, '', this.CoinClick(j + 3), 12.5, 21.484375, 18.484848 + j * 23.560606, 52.083333, 1);
-            taskLevel.setImage("assets/data/img/buttons/task.png", "coin");
+            taskLevel.setImage("assets/data/img/buttons/difficulty_coins/coin_" + (j + 3 + 1) + ".png", "coin");
             me.game.world.addChild(taskLevel);
-            $("#taskLevel" + j + 3).fadeIn(100);
         }
 
         /**
@@ -60,14 +62,14 @@ game.TriviaScreen = me.ScreenObject.extend({
                 $("#taskLevel" + i).fadeOut(100);
             }
             $("#backFromTrivia").fadeOut(100);
+            $("#triviaHeader").fadeOut(100);
             setTimeout( function() {
                 me.state.change(me.state.MENU);
             }, 100);
         };
 
-        var backFromTrivia = new game.ClickableElement('backFromTrivia', 'Back', triviaBackClick, 15, 6.5, 39, 77, 1);
+        var backFromTrivia = new game.ClickableElement('backFromTrivia', 'Back', triviaBackClick, 15, 6.5, 39, 80, 1);
         me.game.world.addChild(backFromTrivia);
-        $("#backFromTrivia").fadeIn(100);
 
     }
 });
