dc8f303a4f1144308af83093861d152172f2b66c
Fabio Mazzone <fabio.mazzone@me.com>
2016-01-21 12:04:44 +0100

    changed datatype of field extension in tabledefinition

--- back-end/app/models/TableDefinition.java
+++ back-end/app/models/TableDefinition.java
@@ -24,7 +24,7 @@ public class TableDefinition extends Model {
     @OneToMany(mappedBy = "tableDefinition", cascade = CascadeType.ALL)
     private List<ColumnDefinition>  columnDefinitions;
 
-    @Column(columnDefinition = "Text")
+    @Column(columnDefinition = "MEDIUMTEXT")
     private final String                  extension;
 
 //////////////////////////////////////////////////
